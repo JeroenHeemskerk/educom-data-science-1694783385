d69f58061de0093aac1a3fb637c231801ccf6a2e
Stijn121 <s.engelmoer@ziggo.nl>
2023-09-19 10:58:58 +0200

    opdracht 4.1.3

--- relational-data-storage/opdracht-4/ds-opdracht-4.1.3.sql
+++ relational-data-storage/opdracht-4/ds-opdracht-4.1.3.sql
@@ -1,8 +1,9 @@
-SELECT id
-FROM mhl_rubrieken WHERE id = 235 AND parent = 235;
-
-SELECT mhl_suppliers_ID
-FROM mhl_suppliers_mhl_rubriek_view WHERE mhl_rubriek_view_ID = 235;
-
-SELECT name, straat, huisnr, postcode 
-FROM mhl_suppliers WHERE city_ID = 104;
+SELECT s.name, s.straat, s.huisnr, s.postcode, 
+FROM mhl_suppliers AS s
+INNER JOIN mhl_suppliers_mhl_rubriek_view AS x
+ON s.id = x.mhl_suppliers_ID
+INNER JOIN mhl_rubrieken AS r
+ON r.id = x.mhl_rubriek_view_ID
+WHERE s.city_ID = 104 AND r.id = 235 
+OR s.city_ID = 104 AND r.parent = 235 
+ORDER BY r.name, s.name
