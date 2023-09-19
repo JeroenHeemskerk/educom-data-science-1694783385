e9deedb0b2d8a2e86a6971906fc5927596ed6ac7
Stijn121 <s.engelmoer@ziggo.nl>
2023-09-19 16:27:44 +0200

    opdracht 4.1.4

--- relational-data-storage/opdracht-4/ds-opdracht-4.1.4.sql
+++ relational-data-storage/opdracht-4/ds-opdracht-4.1.4.sql
@@ -1,9 +1,8 @@
-SELECT s.name, s.straat, s.huisnr, s.postcode, 
+SELECT s.name, s.straat, s.huisnr, s.postcode
 FROM mhl_suppliers AS s
-INNER JOIN mhl_suppliers_mhl_rubriek_view AS x
-ON s.id = x.mhl_suppliers_ID
-INNER JOIN mhl_rubrieken AS r
-ON r.id = x.mhl_rubriek_view_ID
-WHERE s.city_ID = 104 AND r.id = 235 
-OR s.city_ID = 104 AND r.parent = 235 
-ORDER BY r.name, s.name
+INNER JOIN mhl_yn_properties AS y
+ON s.id = y.supplier_ID
+INNER JOIN mhl_propertytypes AS p
+ON y.propertytype_ID = p.id
+WHERE p.name = 'specialistische leverancier' 
+OR p.name = 'ook voor particulieren'
