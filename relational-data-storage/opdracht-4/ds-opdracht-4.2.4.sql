415f0e0c1f1f4ba0d7a8beafbff553c4eb58fdeb
Stijn121 <s.engelmoer@ziggo.nl>
2023-09-20 10:23:08 +0200

    opdracht 5.1.1

--- relational-data-storage/opdracht-4/ds-opdracht-4.2.4.sql
+++ relational-data-storage/opdracht-4/ds-opdracht-4.2.4.sql
@@ -2,7 +2,6 @@ SELECT s.name, p.name, y.content
 FROM mhl_suppliers AS s
 LEFT JOIN mhl_yn_properties AS y
 ON s.id = y.supplier_ID
-LEFT JOIN mhl_propertytypes AS p
+LEFT JOIN (SELECT * FROM mhl_propertytypes WHERE proptype = "A") AS p
 ON y.propertytype_ID = p.id
 WHERE s.city_ID = 104
-ORDER BY s.name
