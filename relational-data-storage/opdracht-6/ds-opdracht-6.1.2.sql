367a0a1f90312d0eddb72beeb0fd49f8af449b7d
Stijn121 <s.engelmoer@ziggo.nl>
2023-09-26 11:30:21 +0200

    opdracht 6.1.2

--- relational-data-storage/opdracht-6/ds-opdracht-6.1.2.sql
+++ relational-data-storage/opdracht-6/ds-opdracht-6.1.2.sql
@@ -1,8 +1,14 @@
 SELECT
-d.name,
+co.name,
 s.name,
 SUM(h.hitcount),
-AVG(h.hitcount)
+(SELECT AVG(h.hitcount) FROM mhl_suppliers AS s
+INNER JOIN mhl_hitcount as h ON s.id = h.supplier_ID
+INNER JOIN mhl_cities as ci ON s.city_ID = ci.id
+INNER JOIN mhl_communes as co2 ON co2.id = ci.commune_ID
+WHERE co2.name = co.name
+GROUP BY co2.name
+ORDER BY AVG(h.hitcount) DESC) as average_hitcount
 FROM mhl_suppliers AS s
 INNER JOIN mhl_hitcount as h
 ON s.id = h.supplier_ID
@@ -10,7 +16,5 @@ INNER JOIN mhl_cities as ci
 ON s.city_ID = ci.id
 INNER JOIN mhl_communes as co
 ON co.id = ci.commune_ID
-INNER JOIN mhl_districts as d
-ON co.district_ID = d.id
-GROUP BY d.name, s.name
-ORDER BY SUM(h.hitcount) DESC, AVG(h.hitcount) DESC
+GROUP BY co.name, s.name
+ORDER BY SUM(h.hitcount) DESC
