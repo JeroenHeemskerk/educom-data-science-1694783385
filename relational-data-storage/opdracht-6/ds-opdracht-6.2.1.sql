d95aed36294efdd84086d0b6914e3f419edc7c1e
Stijn121 <s.engelmoer@ziggo.nl>
2023-09-26 09:24:35 +0200

    opdracht 6.2.1

--- relational-data-storage/opdracht-6/ds-opdracht-6.2.1.sql
+++ relational-data-storage/opdracht-6/ds-opdracht-6.2.1.sql
@@ -1,4 +1,4 @@
-SELECT joindate, id
+SELECT DATE_FORMAT(joindate, "%m.%d.%Y"), id
 FROM mhl_suppliers
 WHERE joindate < LAST_DAY(joindate) AND
-joindate > LAST_DAY(joindate) -7
+joindate > LAST_DAY(joindate) -8
